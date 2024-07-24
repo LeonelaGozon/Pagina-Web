5e979312edecf1e9ecb49b47951ee631867807b4
LeonelaGozon <166418325+LeonelaGozon@users.noreply.github.com>
2024-07-24 11:57:50 -0300

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -3,7 +3,7 @@ function enviarPorWhatsapp() {
     const nombre = document.getElementById("nombre").value;
     const asunto = document.getElementById("asunto").value;
     const mensaje = document.getElementById("mensaje").value;
-    const url = "https://wa.me/2914723804?text="
+    const url = "https://wa.me/2224524332?text="
     + "Nombre: " + nombre + "%0a"
     + "Asunto: " + asunto + "%0a"
     + "Mensaje: " + mensaje;
